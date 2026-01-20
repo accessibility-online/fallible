efef1db0682716033ad6e375bec09160240a7c38
a11ychief <richard.hyman@welcomeally.com>
2026-01-20 21:27:35 +0000

    Made all the methods async in the trait

--- src/s3_facade.rs
+++ src/s3_facade.rs
@@ -16,43 +16,43 @@ impl S3Facade {
 }
 
 impl StorageFacade for  S3Facade {
-    fn read_data<F>(&self, path: &str, decrypt: Option<F>) -> Result<Vec<u8>, Box<dyn std::error::Error>>
+    async fn read_data<F>(&self, path: &str, decrypt: Option<F>) -> Result<Vec<u8>, Box<dyn std::error::Error>>
     where
         F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         todo!()
     }
 
-    fn write_data<F>(&self, path: &str, data: &[u8], encrypt: Option<F>) -> Result<(), Box<dyn std::error::Error>>
+    async fn write_data<F>(&self, path: &str, data: &[u8], encrypt: Option<F>) -> Result<(), Box<dyn std::error::Error>>
     where
         F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         todo!()
     }
 
-    fn list_objects(&self, dir_path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    async fn list_objects(&self, dir_path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
         todo!()
     }
 
-    fn list_object_versions(&self, file_path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    async fn list_object_versions(&self, file_path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
         todo!()
     }
 
-    fn delete_file(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    async fn delete_file(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
         todo!()
     }
 
-    fn move_file(&self, from: &str, to: &str) -> Result<(), Box<dyn std::error::Error>> {
+    async fn move_file(&self, from: &str, to: &str) -> Result<(), Box<dyn std::error::Error>> {
         todo!()
     }
 
-    fn copy_file(&self, from: &str, to: &str) -> Result<(), Box<dyn std::error::Error>> {
+    async fn copy_file(&self, from: &str, to: &str) -> Result<(), Box<dyn std::error::Error>> {
         todo!()
     }
     
-    fn get_file_metadata(&self, path: &str) -> Result<std::fs::Metadata, Box<dyn std::error::Error>> {
+    async fn get_file_metadata(&self, path: &str) -> Result<std::fs::Metadata, Box<dyn std::error::Error>> {
         todo!()
     }
     
-    fn file_exists(&self, path: &str) -> bool {
+    async fn file_exists(&self, path: &str) -> bool {
         todo!()
     }
 }
--- src/storage_facade.rs
+++ src/storage_facade.rs
@@ -15,33 +15,33 @@ use std::fs;
 /// Likewise, where OK values are defined, Standard Library return types are prefered, unless absolutely necessary.
 pub trait StorageFacade {
     /// Reads binary data from a file at a path, optionally takes a decryption function.
-    fn read_data<F>(&self, path: &str, decrypt: Option<F>) -> Result<Vec<u8>, Box<dyn Error>>
+    async fn read_data<F>(&self, path: &str, decrypt: Option<F>) -> Result<Vec<u8>, Box<dyn Error>>
     where
         F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn Error>>;
 
         /// Writes binary data to a file at a path, optionally takes an encryption function.
-    fn write_data<F>(&self, path: &str, data: &[u8], encrypt: Option<F>) -> Result<(), Box<dyn Error>>
+    async fn write_data<F>(&self, path: &str, data: &[u8], encrypt: Option<F>) -> Result<(), Box<dyn Error>>
     where
         F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn Error>>;
 
     /// Lists files at a given directory path
-    fn list_objects(&self, dir_path: &str) -> Result<Vec<String>, Box<dyn Error>>;
+    async fn list_objects(&self, dir_path: &str) -> Result<Vec<String>, Box<dyn Error>>;
 
     /// Lists versions of a file at a filepath, originally intended for buckets but custom filesystem implementations are welcome
-    fn list_object_versions(&self, file_path: &str) -> Result<Vec<String>, Box<dyn Error>>;
+    async fn list_object_versions(&self, file_path: &str) -> Result<Vec<String>, Box<dyn Error>>;
 
     /// Deletes a file at a filepath
-    fn delete_file(&self, path: &str) -> Result<(), Box<dyn Error>>;
+    async fn delete_file(&self, path: &str) -> Result<(), Box<dyn Error>>;
 
     /// Moves a file from one location to another, both paths must include the filename to facilitate renaming
-    fn move_file(&self, from: &str, to: &str) -> Result<(), Box<dyn Error>>;
+    async fn move_file(&self, from: &str, to: &str) -> Result<(), Box<dyn Error>>;
 
     /// Copies a file from one location to another, both paths must include the filename to facilitate rename on copy
-    fn copy_file(&self, from: &str, to: &str) -> Result<(), Box<dyn Error>>;
+    async fn copy_file(&self, from: &str, to: &str) -> Result<(), Box<dyn Error>>;
 
     /// Returns a standard metadata object for a file at a given path
-    fn get_file_metadata(&self, path: &str) -> Result<fs::Metadata, Box<dyn Error>>;
+    async fn get_file_metadata(&self, path: &str) -> Result<fs::Metadata, Box<dyn Error>>;
 
     /// Checks if a file exists at a given path, cannot be used for directories
-    fn file_exists(&self, path: &str) -> bool;
+    async fn file_exists(&self, path: &str) -> bool;
 }
