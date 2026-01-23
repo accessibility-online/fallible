cf691f813f4b94309927e77570a6650955e8ab0d
a11ychief <richard.hyman@welcomeally.com>
2026-01-23 18:32:27 +0000

    Added first implementation of new and read data functions for s3 facade

--- src/s3_facade.rs
+++ src/s3_facade.rs
@@ -1,8 +1,9 @@
 // Provides abstractions for calling layers to interface with AWS s3
 // 
 // More to follow
-use crate::storage_facade::{StorageFacade, StoreMetadata};
-use aws_sdk_s3 as s3;
+use crate::storage_facade::{DataStoreId, StorageFacade, StoreMetadata};
+use aws_sdk_s3::{self as s3, Config};
+use aws_config as aws;
 
 /// Contains the client and metadata as fields
 pub struct S3Facade {
@@ -10,11 +11,42 @@ pub struct S3Facade {
     metadata: StoreMetadata,
 }
 
+impl S3Facade {
+    pub async fn new(name: &str, description: &str) -> Self {
+        let config = aws::load_defaults(aws::BehaviorVersion::v2026_01_12()).await;
+        let client = s3::Client::new(&config);
+
+        // ToDo: Check bucket with name exists in the account
+
+S3Facade {
+    client,
+    metadata: StoreMetadata {
+        id: DataStoreId::S3("".to_string()),
+        name: name.to_string(),
+        description: Some(description.to_string())
+    }
+}
+    }
+}
+
 impl StorageFacade for S3Facade {
     async fn read_data<F>(&self, path: &str, decrypt: Option<F>) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>>
     where
         F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> + Send + Sync {
-        todo!()
+        let data = self.client.get_object()
+        .bucket(&self.metadata.name)
+        .key(path)
+        .send()
+        .await?;
+
+    let bytes = data.body.collect().await?.into_bytes();
+
+    if let Some(decrypt_fn) = decrypt {
+        println!("Encryption is currently not implemented, returning raw data.");
+        return Ok(Vec::from(bytes));
+    }
+    
+        Ok(Vec::from(bytes))
     }
 
     async fn write_data<F>(&self, path: &str, data: &[u8], encrypt: Option<F>) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
